f25841a62882a7087e4945a52f5a0dca507a8dfe
Adam Murphy <oxnard@me.com>
2013-07-01 13:36:27 -0700

    Add authorization via CanCan

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.13'
 gem 'devise'
 gem 'haml'
 gem 'heroku'
+gem "cancan"
 group :production do
   gem 'pg'
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,19 @@ class HomeController < ApplicationController
     before_filter :authorize_user!, :except => [:index]
     def index
     end
-    
-    def ror 
-         render "ror_content"   
+
+    def ror
+         render "ror_content"
     end
-    
- 
+
+    # def edit
+    #   @home = Home.find(params[:id])
+    #   authorize! :read, @home
+    # end
+
+    # def update
+    #   @home = Home.find(params[:id])
+    #   authorize! :read, @home
+    # end
+
 end
