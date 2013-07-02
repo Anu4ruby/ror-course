28c60f318fa36b6727fbcf54cc30d2b63bccbc38
Pu Chen <pobuchan2010@gmail.com>
2013-07-02 12:30:48 -0700

    add associations and validates to models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ActiveRecord::Base
   attr_accessible :created_by, :description, :type
+  has_many :answers
+  has_many :choices
+  validates :description, :presence => true
+  validates :type, :presence => true
+  validates :created_by, :presence => true
 end
