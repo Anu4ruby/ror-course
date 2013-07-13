89ea6ab72e72c625925511c385828977d2454bd5
Pu Chen <pobuchan2010@gmail.com>
2013-07-12 18:34:00 -0700

    add some question_mailer test cases

---
+++ spec/mailers/question_mailer_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe QuestionMailer do
+  before(:each) do
+    @question = FactoryGirl.create(:responded_visitor_question)
+    @mail = QuestionMailer.responded(@question)
+  end
+  it 'renders the subject' do
+    @mail.subject.should == 'Your question has been answered'
+    # true
+  end
+  it 'renders the sender email' do
+    @mail.from.should == ['info@ror-course.com']
+    # true
+  end
+  it 'renders the receiver email' do
+    @mail.to.should == [@question.email]
+    # true
+  end
+  it 'assigned show_ask_url(@question)' do
+    @mail.body.encoded.should =~ /#{show_ask_url(@question)}/
+    # true
+  end
+end
