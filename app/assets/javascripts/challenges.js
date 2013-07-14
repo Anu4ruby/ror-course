c36bdecd86c5106f13d599fb038ebc91c81e4768
Pu Chen <pobuchan2010@gmail.com>
2013-07-14 12:04:09 -0700

    ui tweak, more consistent

--- app/assets/javascripts/challenges.js
+++ app/assets/javascripts/challenges.js
@@ -3,34 +3,16 @@
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 $(function(){
 	triggerQuestionType();
-	addChoiceSelection();
-	addAnswerAsChoice();
 	toggleSingleSelectAnswer();
 });
 function triggerQuestionType(){
 	$(document).on('change', '#question_qtype', function(){
 		$.get('/challenges/new?type='+$(this).val(), function(data){
-			//alert(data);
-			$('.form').replaceWith(data);
+			// alert($(data).find('.choices .fields').eq(0).html());
+			$('#page-content > .container').html(data);
 		});
 	});
 	$('#question_type').change();
-}
-function addChoiceSelection(){
-	$(document).on('blur', '.choices [name$="[description]"]', function(){
-		var choice = $(this).val();
-		//alert(choice);
-	});
-}
-function addAnswerAsChoice(){
-	$(document).on('blur', '.answers [name$="[description]"]', function(){
-		var text = $(this).val();
-		if(text.length > 0){
-			$('form .choices a.add_nested_fields').click();
-			$('form .choices .fields').last().find('input').eq(0).val(text);
-		}
-			
-	});
 }
 function toggleSingleSelectAnswer(){
 	$(document).on('click', '.choices input[type=radio]', function(){
