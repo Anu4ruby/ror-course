d1b30f1ad502e7b8d9fd6208add26053cfb11666
Pu Chen <pobuchan2010@gmail.com>
2013-07-14 19:08:29 -0700

    app helper: modified and create spec file

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+describe ApplicationHelper do
+  context 'check assets' do
+    before(:each) do
+    end
+    it 'should has asset' do
+      
+    end
+    it 'should not has asset' do
+      
+    end
+  end
+end
