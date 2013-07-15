8722021d04e528b903de2780de9cf46e51060512
Pu Chen <pobuchan2010@gmail.com>
2013-07-15 09:55:21 -0700

    Spec app helper: more test cases

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,13 +1,31 @@
 require 'spec_helper'
 describe ApplicationHelper do
   context 'check assets' do
-    before(:each) do
+    it 'should work' do
+      asset_exist?('application.js').should == true
     end
-    it 'should has asset' do
-      
+    it 'should not work' do
+      asset_exist?(Time.now.to_time.to_i.to_s + '.js').should == false
     end
-    it 'should not has asset' do
-      
+  end
+  context 'get page_items' do
+    before(:all) do
+      @collection = (1..105).to_a
+    end
+    it 'should return [] for page 0' do
+      page_items(@collection, 0).should == []
+    end
+    it 'should return 1..10 for page 1' do
+      page_items(@collection, 1).should == (1..10).to_a
+    end
+    it 'should return 51..60 for page 6' do
+      page_items(@collection, 6).should == (51..60).to_a
+    end
+    it 'should return 101..105 for page 11 which is last page' do
+      page_items(@collection, 11).should == (101..105).to_a
+    end
+    it 'should return [] for page 12 which is > max page' do
+      page_items(@collection, 12).should == []
     end
   end
 end
