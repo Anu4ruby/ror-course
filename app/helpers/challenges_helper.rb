cde3a03efa627f98d2da093cb7ae40ee868dee63
Pu Chen <pobuchan2010@gmail.com>
2013-07-21 23:09:52 -0400

    challenge: helper+spec update

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -1,19 +1,35 @@
 module ChallengesHelper
   def setup_question(question)
-    if question.new_record?
-      if question.qtype.nil?
-        question.qtype = 'text' 
-      end
-      if !question.type?('text')
-        (5-question.options.size).times { question.options.new()}
-      else
-        question.options.new(selected:true) 
-      end
-    end
+    init_type!(question)
+    opts = question.options
+    question.type?('text') ? set_text_options!(opts) : set_selection_options!(opts) 
     question
   end
-  
+  def init_type!(question)
+    question.qtype = 'text' if !question_types.has_value?(question.qtype) 
+  end
+  def set_text_options!(options)
+    opt = options.first
+    opt.nil? ? (opt = Option.new(:selected => true)) : (opt.selected = true)
+    options.clear << opt
+  end
+  def set_selection_options!(options)
+    (5-options.size).times.inject(options){|result| result << Option.new}
+  end
   def question_types
-    [['Free Text', 'text'],['Single Select', 'single-select'],['Multi Select', 'multi-select']]
+    {'Free Text' => 'text', 'Single Select' => 'single-select', 'Multiple Select' => 'multi-select'}
+  end
+  
+  def check_answers(questions, answers)
+    correct = 0
+    pending = 0
+    questions.each_with_index do |q, idx|
+      if q.type?('text')
+        pending += 1
+        next
+      end
+      correct +=1 if q.check_answer(answers[q.id.to_s])
+    end
+    {:size => questions.size, :correct => correct, :pending => pending}
   end
 end
--- spec/helpers/challenges_helper_spec.rb
+++ spec/helpers/challenges_helper_spec.rb
@@ -2,44 +2,136 @@ require 'spec_helper'
 describe ChallengesHelper do
   context 'questions types' do
     it 'has hash of all question types with lable=>value' do
-      types = [['Free Text', 'text'],['Single Select', 'single-select'],['Multi Select', 'multi-select']]
-      question_types().should == types
+      types = { 'Free Text' => 'text', 
+                'Single Select' => 'single-select', 
+                'Multiple Select' => 'multi-select'}
+      question_types.should == types
     end
     
   end
-  context 'setup question' do
-    context 'type of "text"' do
-      before(:each) do
-        @q = Question.new
-        @q1 = setup_question(@q)
+  describe 'setup question' do
+    
+    describe 'should change' do
+      it 'new record' do
+        question = Question.new
+        dup = question.clone
+        setup_question(question)
+        question.should_not == dup
       end
-      it 'should return a question of type "text"' do
-        @q.qtype = 'text'
-        @q1.should == @q
-      end
-      it 'should has only 1 option' do
-        @q.options.new(:selected => true)
-        @q1.options == @q.options
-      end
-    end
-    context 'type of "single-select"' do
-      before(:each) do
-        @q = Question.new(:qtype => 'single-select')
-        @q1 = setup_question(@q)
-      end
-      it 'should return a question of type "single-select"' do
-        @q1.should == @q
-      end
-      it 'should has 5 empty options' do
-        5.times{@q.options.new}
-        @q1.options == @q.options
+      describe 'with defined type' do
+        ['text','single-select', 'multi-select'].each do |type|
+          it "#{type}" do
+            question = Question.new(:qtype => type)
+            dup = question.dup
+            setup_question(question)
+            question.should_not == dup
+          end
+        end  
       end
     end
-    context 'type of "multi-select"' do
-      it 'should return a question of type "multi-select"' do
-        setup_question(Question.new(:qtype => 'multi-select'))
-        true
-      end  
+    describe 'should not change' do
+      it 'text with options of first with content and selected' do
+        question = FactoryGirl.build(:text_question)
+        dup = question.clone
+        setup_question(question)
+        question.options.should == dup.options
+        
+        question.should be_type(dup.qtype)
+        question.description.should == dup.description
+      end
+      
+    end
+    
+  end
+  
+  describe 'init_type!' do
+    before(:each) {@question = Question.new}
+    
+    it 'new record match type text' do
+      init_type!(@question)
+      @question.should be_type('text')
+    end
+    describe 'with predefined type' do
+      ['text', 'single-select', 'multi-select'].each do |type|
+        it "matches #{type}" do
+          @question.qtype = type
+          init_type!(@question)
+          @question.should be_type(type)
+        end
+      end
+      it 'matches text with invalid type' do
+        @question.qtype = 'long'
+        init_type!(@question)
+        @question.should be_type('text')
+      end
+    end
+  end
+  describe 'set_text_options!' do
+    
+    context 'always size of 1' do
+      [0, 1, 5].each do |number|
+        it "starts with #{number} options" do
+          options = number.times.inject([]){|result| result << Option.new }
+          set_text_options!(options)
+          options.size.should == 1
+        end
+      end
+    end
+    
+    context 'will be selected' do
+      it 'come with selected' do
+        options = [Option.new(:selected => true)]
+        set_text_options!(options)
+        options.first.should be_selected
+      end
+      it 'come without selected' do
+        options = [Option.new]
+        set_text_options!(options)
+        options.first.should be_selected
+      end
+    end
+    
+    context 'will not change content' do
+      ['', 'some content'].each do |content|
+        it "with '#{content}'" do
+          options = [Option.new(:content => content)]
+          set_text_options!(options)
+          options.first.content.should == content
+        end
+      end
+    end
+  end
+
+  describe 'set_selection_options!' do
+    context 'at least 5 options' do
+      [0, 1, 5, 8].each do |number|
+        it "start with #{number} options" do
+          options = number.times.inject([]){|result| result << Option.new }
+          set_selection_options!(options)
+          options.size.should >= 5
+        end
+      end
+    end
+    
+    context 'fill with extract options if options < 5' do
+      [0, 1, 4].each do |number|
+        it "starts with #{number} options with content" do
+          options = number.times.inject([]){|result| result << Option.new(content: 'content') }
+          set_selection_options!(options)
+          options.select{|o| o.content.blank?}.size.should == (5-number)
+        end
+      end
+    end
+    
+    context 'no change if options >= 5' do
+      [5, 8, 12].each do |number|
+        it "starts with #{number} options" do
+          options = number.times.inject([]){|result| result << Option.new(content: 'content') }
+          dup = options.dup
+          set_selection_options!(options)
+          options.should == dup
+        end
+      end
     end
     
   end
