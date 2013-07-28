f03189132a46c9247993b05866d7a7641bfb5c2f
Pu Chen <pobuchan2010@gmail.com>
2013-07-28 00:12:58 -0400

    spec: test_helper update

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,16 +1,25 @@
 module TestHelper
-  # for login user
-  def set_user(user=nil)
-    if !user.nil?
-      set_current_user(user)
-      set_user_signed_in(true)  
-    else
-      set_user_signed_in(false)
-    end
+  def set_current_user(user)
+    subject.stub(:current_user).and_return(user)
   end
+  
+  def set_user_signed_in(confirm=false)
+    subject.stub(:user_signed_in?).and_return(confirm)
+  end
+  
   def clear_user
     set_current_user(nil)
     set_user_signed_in
+  end
+  
+  # for login user
+  def set_user(user=nil)
+    if user.nil?
+      set_user_signed_in(false)
+    else
+      set_current_user(user)
+      set_user_signed_in(true)
+    end
   end
   
   #login alternative
@@ -25,46 +34,23 @@ module TestHelper
     admin
   end
   
-  def set_current_user(user)
-    controller.stub(:current_user).and_return(user)
-    # controller.should_receive(:current_user).at_least(0).times.and_return(user)
-  end
-  def set_user_signed_in(confirm=false)
-    controller.stub(:user_signed_in?).and_return(confirm)
-    # controller.should_receive(:user_signed_in).at_least(0).times.and_return(confirm)
+  # give admin permission if it states in spec file
+  #   let(:current_user) { FactoryGirl.create(:admin) }
+  #   let(:user_signed_in?) { true }
+  # if not concern of using current_user? and user_signed_in?, use bellow  
+  def pass_authorize
+    subject.should_receive(:authorize_user!).at_least(0).times.and_return(nil)
   end
   
-  # these code works if it states in spec file
-  #   let(:current_user){FactoryGirl.create(:admin)}
-  #   let(:user_signed_in?){return true}
-  #   # if not concern of using current_user and user_signed_in, ignore above  
-  #   before(:each) do
-  #     controller.should_receive(:authorize_user!).at_least(0).times.and_return(nil)
-  #   end
-  def pass_authorize
-    controller.should_receive(:authorize_user!).at_least(0).times.and_return(nil)
-  end
   # shortcut for factory girl
-  # # factorygirl create
-  # def fgc(symbol, options={})
-    # FactoryGirl.create(symbol.to_sym, options)
-  # end
-  # # factorygirl build
-  # def fgb(symbol, options={})
-    # FactoryGirl.build(symbol.to_sym, options)
-  # end
-  # # factorygirl attributes_for
-  # def fga(symbol, options={})
-    # FactoryGirl.attributes_for(symbol.to_sym, options)
-  # end
   def method_missing(meth, *args, &block)
     if m = meth.to_s.match(/^fg([abc])$/)
       case m[1]
-      when 'a'
+      when 'a' #attributes_for
         FactoryGirl.attributes_for(*args, &block)
-      when 'b'
+      when 'b' #build
         FactoryGirl.build(*args, &block)
-      when 'c'
+      when 'c' #create
         FactoryGirl.create(*args, &block)
       end
     else
